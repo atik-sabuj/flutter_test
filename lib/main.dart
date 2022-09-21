08a24ec407cf552dd583c4fd67131ed48e43d473
Atikuzzaman Sabuj <sabuj235@gmail.com>
2022-09-21 23:12:46 +0600

    This is my 3rd project of android. its called Sajek Valley.Basically, i just change the text only.

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
         home: Scaffold(
           appBar: AppBar(
-            title: Text("sdlfkasjlfkjlasdjflk"),
+            title: Text("Hello Android "),
           ),
           body: Sajek(),
         ),
